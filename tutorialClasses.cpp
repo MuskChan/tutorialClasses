8e08096432687c96190ad0088477f10e347bb114
musk <musk.chan.c@gmail.com>
2020-04-09 16:38:32 +0800

    pointer to classes example

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,25 +1,27 @@
-// member initialization
+// pointer to classes example
 #include <iostream>
 using namespace std;
 
-class Circle {
-    double radius;
+class Rectangle {
+    int width, height;
 public:
-    Circle(double r) : radius(r) { }
-    double area() { return radius * radius * 3.14159265; }
+    Rectangle(int x, int y) : width(x), height(y) {}
+    int area(void) { return width * height; }
 };
 
-class Cylinder {
-    Circle base;
-    double height;
-public:
-    Cylinder(double r, double h) : base(r), height(h) {}
-    double volume() { return base.area() * height; }
-};
 
 int main() {
-    Cylinder foo(10, 20);
-
-    cout << "foo's volume: " << foo.volume() << '\n';
+    Rectangle obj(3, 4);
+    Rectangle* foo, * bar, * baz;
+    foo = &obj;
+    bar = new Rectangle(5, 6);
+    baz = new Rectangle[2]{ {2,5}, {3,6} };
+    cout << "obj's area: " << obj.area() << '\n';
+    cout << "*foo's area: " << foo->area() << '\n';
+    cout << "*bar's area: " << bar->area() << '\n';
+    cout << "baz[0]'s area:" << baz[0].area() << '\n';
+    cout << "baz[1]'s area:" << baz[1].area() << '\n';
+    delete bar;
+    delete[] baz;
     return 0;
 }
