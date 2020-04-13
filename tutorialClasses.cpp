aeefacf61b981043564c1b02f296db34f23f8132
musk <musk.chan.c@gmail.com>
2020-04-13 14:59:31 +0800

    more pointers

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,16 +1,27 @@
-﻿// my first pointer
+﻿// more pointers
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int firstvalue, secondvalue;
-    int* mypointer;
+    int firstvalue = 5, secondvalue = 15;
+    int* p1, * p2;
 
-    mypointer = &firstvalue; //mypointer使用地址运算符（&）分配了firstvalue的地址
-    *mypointer = 10; //mypointer所指向的值分配为10  此时mypointer指向的存储位置为firstvalue 所以实际上修改了firstvalue的值
-    mypointer = &secondvalue;
-    *mypointer = 20;
+
+    cout << "dd  " << p2 << '\n';
+
+    p1 = &firstvalue;  // p1 = address of firstvalue
+    p2 = &secondvalue; // p2 = address of secondvalue
+    *p1 = 10;          // value pointed to by p1 = 10
+
+    *p2 = *p1;         // value pointed to by p2 = value pointed to by p1
+
+    p1 = p2;           // p1 = p2 (value of pointer is copied)
+
+    *p1 = 20;          // value pointed to by p1 = 20
+
+    cout << "test  " << *p1 << '\n';
+
     cout << "firstvalue is " << firstvalue << '\n';
     cout << "secondvalue is " << secondvalue << '\n';
     return 0;
