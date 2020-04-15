d0435dc3b870e036354d63bdde138177a77486c0
musk <musk.chan.c@gmail.com>
2020-04-15 16:12:18 +0800

    my first program in C++

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,17 +1,7 @@
-﻿// more pointers
+﻿// my first program in C++
 #include <iostream>
-using namespace std;
 
 int main()
 {
-    int numbers[5];
-    int* p; //声明指针
-    p = numbers;  *p = 10;
-    p++;  *p = 20;
-    p = &numbers[2];  *p = 30;
-    p = numbers + 3;  *p = 40;
-    p = numbers;  *(p + 4) = 50;
-    for (int n = 0; n < 5; n++)
-        cout << numbers[n] << ", ";
-    return 0;
+	std::cout << "hello world";
 }
