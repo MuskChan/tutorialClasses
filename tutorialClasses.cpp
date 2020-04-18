313cac215a4be268c40fed831543ad26faedffcf
musk <musk.chan.c@gmail.com>
2020-04-19 07:24:27 +0800

    declaring variables 变量声明

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,9 +1,23 @@
-﻿// Using namespace std
+﻿// operating with variables
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	cout << "hello world";
-	cout << "i am a C++ program";
+	//declaring variables 变量声明
+	int a, b;
+	int result;
+
+	// process
+	a = 5;
+	b = 2;
+	a = a + 1;
+	result = a - b;
+
+	//  print out the result;
+	cout << result;
+
+	//terminate the program:
+	return 0;
 }
