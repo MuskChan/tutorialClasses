b1e1853b59542f3a37b97cee2c7ba9b7da2854a9
musk <musk.chan.c@gmail.com>
2020-04-20 15:38:16 +0800

    Typed constant expressions 键入常量表达式

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,24 +1,15 @@
-﻿// my first string 
-#include <iostream>
-#include <string>
+﻿#include <iostream>
 using namespace std;
 
-int  main()
+const double pi = 3.14159;
+const char newline = '\n';
+
+int main()
 {
-	string mystring;
-	string mystring2 ("  this is a mystring2");
-	string mystring3 {"  this is a mystring3" };
+	double r = 5.0;
+	double circle;
 
-	//与基本类型一样，所有初始化格式对字符串均有效：
-	mystring = "this is a string";
-
-	cout << mystring;
-	cout << mystring2;
-	cout << mystring3;
-
-	//换行符 endl
-	cout << mystring << endl;
-	mystring = "This is a different string content";
-	cout << mystring << endl;
-	return 0;
+	circle = 2 * pi * r;
+	cout << circle;
+	cout << newline;
 }
