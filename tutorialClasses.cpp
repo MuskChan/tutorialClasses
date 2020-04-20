ac2d83218a2bfa7caeae998adc9ce063007af518
musk <musk.chan.c@gmail.com>
2020-04-20 16:01:41 +0800

    assignment operator

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,16 +1,17 @@
-﻿#include <iostream>
+﻿// assignment operator 
+#include <iostream>
 using namespace std;
-
-#define	PI 3.14159
-#define NEWLINE '\n'
-//请注意，这些#define行是预处理程序指令，因此是单行指令，与C ++语句不同，该行最后不需要分号（;）；该指令自动扩展到行尾。如果该行中包含分号，则它是替换序列的一部分，并且也包含在所有替换的事件中。
 
 int main()
 {
-	double r = 5.0;
-	double circle;
+	int a, b;
+	a = 10;
+	b = 4;
+	a = b;
+	b = 7;
 
-	circle = 2 * PI * r;
-	cout << circle;
-	cout << NEWLINE;
+	cout << "a:";
+	cout << a;
+	cout << " b:";
+	cout << b;
 }
