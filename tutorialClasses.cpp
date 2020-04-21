6c8679861bb761307b196d9581f204095f28d6e0
musk <musk.chan.c@gmail.com>
2020-04-21 10:21:50 +0800

    cin with strings

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,14 +1,16 @@
-﻿// io example 
-
+﻿// cin with strings
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	int i;
-	cout << "please enter an integer value: ";
-	cin >> i;
-	cout << "the value you entered is " << i;
-	cout << " and its double is " << i * 2 << ".\n";
+	string mystr;
+	cout << "What's your name? ";
+	getline(cin, mystr);
+	cout << "Hello " << mystr << ".\n";
+	cout << "What is you favorite team? ";
+	getline(cin, mystr);
+	cout << "I like " << mystr << " too!\n";
 	return 0;
 }
