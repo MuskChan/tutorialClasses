53b0343bf869fe52a636c5a1a50bbbd162d0de8e
musk <musk.chan.c@gmail.com>
2020-04-22 11:32:22 +0800

    custom countdown using while

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,21 +1,15 @@
-﻿// stringstreams
+﻿// custom countdown using while 
 #include <iostream>
-#include <string>
-#include <sstream>
 using namespace std;
 
 int main()
 {
-	string mystr;
-	float price = 0;
-	int quantity = 0;
+	int n = 10;
 
-	cout << "Enter price: ";
-	getline(cin, mystr);
-	stringstream(mystr) >> price;
-	cout << "Enter quantity: ";
-	getline(cin, mystr);
-	stringstream(mystr) >> quantity;
-	cout << "Total price: " << price * quantity << endl;
-	return 0;
+	while (n > 0) {
+		cout << n << ", ";
+		--n;
+	}
+
+	cout << "liftoff!\n";
 }
