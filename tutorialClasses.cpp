738284e5308d883930b3f77574ef1a6afae86ec5
musk <musk.chan.c@gmail.com>
2020-04-29 17:02:49 +0800

    non-member operator overloads

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,19 +1,18 @@
-﻿// overloading operators example 
+﻿// non-member operator overloads
 #include <iostream>
 using namespace std;
 
 class CVector {
 	public:
 		int x, y;
-		CVector() {};
+		CVector () {}
 		CVector (int a, int b) : x(a), y(b) {}
-		CVector operator + (const CVector&);
 };
 
-CVector CVector::operator+ (const CVector& param) {
+CVector operator+ (const CVector& lhs, const CVector& rhs) {
 	CVector temp;
-	temp.x = x + param.x;
-	temp.y = y + param.y;
+	temp.x = lhs.x + rhs.x;
+	temp.y = lhs.y + rhs.y;
 	return temp;
 }
 
