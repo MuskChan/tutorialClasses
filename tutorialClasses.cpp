be5af21081bbcbb9fe8bc4f8e3b2a54ea6dafae0
musk <musk.chan.c@gmail.com>
2020-04-29 15:52:14 +0800

    overloading operators example

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,14 +1,27 @@
-﻿// echo machine
+﻿// overloading operators example 
 #include <iostream>
-#include <string>
 using namespace std;
 
-int main()
-{
-	string str;
-	do {
-		cout << "Enter text: ";
-		getline(cin, str);
-		cout << "You entered: " << str << '\n';
-	} while (str != "goodbye");
+class CVector {
+	public:
+		int x, y;
+		CVector() {};
+		CVector (int a, int b) : x(a), y(b) {}
+		CVector operator + (const CVector&);
+};
+
+CVector CVector::operator+ (const CVector& param) {
+	CVector temp;
+	temp.x = x + param.x;
+	temp.y = y + param.y;
+	return temp;
+}
+
+int main() {
+	CVector foo(3, 1);
+	CVector bar(1, 2);
+	CVector result;
+	result = foo + bar;
+	cout << result.x << ',' << result.y << '\n';
+	return 0;
 }
