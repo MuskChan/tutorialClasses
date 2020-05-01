2cafe3194eda1f152d0c17366c6c146fe7228a76
musk <musk.chan.c@gmail.com>
2020-05-01 11:03:53 +0800

    classes example

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,26 +1,23 @@
-﻿// non-member operator overloads
+﻿// classes example 
 #include <iostream>
 using namespace std;
 
-class CVector {
+class Rectangle { //另一种写法 struct
+		int width, height;
 	public:
-		int x, y;
-		CVector () {}
-		CVector (int a, int b) : x(a), y(b) {}
+		void set_values(int, int);
+		int area() { return width * height; }
 };
 
-CVector operator+ (const CVector& lhs, const CVector& rhs) {
-	CVector temp;
-	temp.x = lhs.x + rhs.x;
-	temp.y = lhs.y + rhs.y;
-	return temp;
+//在此外部定义中，作用域（::）的运算符用于指定所定义的函数是该类的成员
+void Rectangle::set_values(int x, int y) {
+	width = x;
+	height = y;
 }
 
 int main() {
-	CVector foo(3, 1);
-	CVector bar(1, 2);
-	CVector result;
-	result = foo + bar;
-	cout << result.x << ',' << result.y << '\n';
+	Rectangle rect;
+	rect.set_values(3, 4);
+	cout << "area: " << rect.area();
 	return 0;
 }
