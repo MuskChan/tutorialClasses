be5123509b101844c996957c9634779faaac68db
musk <musk.chan.c@gmail.com>
2020-05-01 11:19:39 +0800

    example: one class, two objects

--- tutorialClasses.cpp
+++ tutorialClasses.cpp
@@ -1,23 +1,24 @@
-﻿// classes example 
+﻿// example: one class, two objects
 #include <iostream>
 using namespace std;
 
-class Rectangle { //另一种写法 struct
-		int width, height;
-	public:
-		void set_values(int, int);
-		int area() { return width * height; }
+class Rectangle {
+	int width, height;
+public:
+	void set_values(int, int);
+	int area() { return width * height; }
 };
 
-//在此外部定义中，作用域（::）的运算符用于指定所定义的函数是该类的成员
 void Rectangle::set_values(int x, int y) {
 	width = x;
 	height = y;
 }
 
 int main() {
-	Rectangle rect;
+	Rectangle rect, rectb;
 	rect.set_values(3, 4);
-	cout << "area: " << rect.area();
+	rectb.set_values(5, 6);
+	cout << "rect area: " << rect.area() << endl;
+	cout << "rectb area: " << rectb.area() << endl;
 	return 0;
 }
